5a12da7c0fd708449cf2b85f06cd9db15654f63c
abdulrafey07 <a.rafey4190@gmail.com>
2025-07-04 10:34:21 +0530

    made footer of the main page & basic layout of Query form

--- script.js
+++ script.js
@@ -8,4 +8,31 @@ document.querySelectorAll(".op").forEach((box)=> {
 });
 
 
+document.querySelectorAll(".nav-links li").forEach((e) => {
+    e.addEventListener("click", ()=> {
+        let link2 = e.getAttribute("data-link");
+        if(link2){
+            window.location.href = link2;
+        }
+    });
+});
 
+
+let logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=> {
+        let link3 = logo.getAttribute("data-link");
+        if(link3){
+            window.open(link3, "_self");
+        }
+    });
+
+
+let footerContent = document.querySelectorAll(".footerContent");
+footerContent.forEach( (e)=> {
+    footerContent.addEventListener("click", ()=> {
+        let link = e.getAttribute("data-link");
+        if(link){
+            window.open(link, "_blank");
+        }
+    });
+});
